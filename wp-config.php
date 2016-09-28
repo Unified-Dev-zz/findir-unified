62773d7168e9ffff3085e1d070d5db829172aabe
Unified-Dev <saikat@unifiedinfotech.net>
2016-09-28 19:39:11 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_PASSWORD', '4pS(eS4[72');
 
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '69.175.35.82');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
